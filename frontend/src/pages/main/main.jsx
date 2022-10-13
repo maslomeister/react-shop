b7844db71827037b6afaeee7f1368588524eec61
maslomeister <maslomeister@gmail.com>
2022-10-14 01:40:34 +0400

    refactor: code cleanup

--- frontend/src/pages/main/main.jsx
+++ frontend/src/pages/main/main.jsx
@@ -2,45 +2,44 @@ import React from "react";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-import { clearProduct } from "../../store/actions/shop";
+import { cartDataError, cartDataRequest, cartDataSuccess, clearProduct } from "../../store/actions/shop";
 import { ShopItem } from "../../components/shop-item/shop-item";
-import { Spinner } from "../../components/spinner/spinner.jsx";
 
 import styles from "./main.module.css";
+import { fetchCartApi } from "../../api/api";
 
 export const Main = () => {
   const dispatch = useDispatch();
-  const { products, productsLoading } = useSelector((state) => state.shop);
+  const { authToken, authenticated, isUser } = useSelector((state) => state.auth);
+  const { products } = useSelector((state) => state.shop);
 
   useEffect(() => {
     dispatch(clearProduct());
   }, []);
 
+  useEffect(() => {
+    if (authenticated && isUser) {
+      fetchCartApi(authToken, dispatch, cartDataRequest, cartDataSuccess, cartDataError);
+    }
+  }, []);
+
   return (
     <main className={styles.main}>
-      {productsLoading && (
-        <div className={styles["spinner-container"]}>
-          <Spinner />
-        </div>
-      )}
-
-      {!productsLoading && (
-        <div className={styles.container}>
-          {products.length > 0 &&
-            products.map((product) => {
-              return (
-                <ShopItem
-                  id={product.id}
-                  img={product.picture}
-                  name={product.name}
-                  price={product.price}
-                  inStock={product.inStock}
-                  key={product.id}
-                />
-              );
-            })}
-        </div>
-      )}
+      <div className={styles.container}>
+        {products.length > 0 &&
+          products.map((product) => {
+            return (
+              <ShopItem
+                id={product.id}
+                img={product.picture}
+                name={product.name}
+                price={product.price}
+                inStock={product.inStock}
+                key={product.id}
+              />
+            );
+          })}
+      </div>
     </main>
   );
 };
