944650e876e7f2a17093da1cfaeb2b0f3ddaf193
maslomeister <maslomeister@gmail.com>
2022-10-14 01:36:12 +0400

    feat: readOnly and labelColor props

--- frontend/src/components/input/input.jsx
+++ frontend/src/components/input/input.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 import styles from "./input.module.css";
 
-export const Input = ({ type, placeholder, name, value, label, maxLength, onChange, onBlur, onFocus, error }) => {
+export const Input = ({ type, placeholder, name, value, label, labelColor, maxLength, onChange, onBlur, onFocus, error, readOnly }) => {
   return (
     <div className={styles["input-container"]}>
-      <label className={styles.label}>{label}</label>
+      {label && (
+        <label className={styles.label} style={{ color: labelColor }}>
+          {label}
+        </label>
+      )}
       <input
         className={`${styles.input} ${value.length > 0 ? styles.active : ""}`}
         type={type}
@@ -16,6 +20,7 @@ export const Input = ({ type, placeholder, name, value, label, maxLength, onChan
         onFocus={onFocus}
         name={name}
         maxLength={maxLength}
+        readOnly={readOnly}
       />
       <label className={styles.error}>{error}</label>
     </div>
