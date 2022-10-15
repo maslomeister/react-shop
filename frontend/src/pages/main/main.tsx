42826eaa896671b5e78b8c426e7fea0777133c6b
maslomeister <maslomeister@gmail.com>
2022-10-15 21:55:57 +0400

    fix: spinner for products loading

--- frontend/src/pages/main/main.tsx
+++ frontend/src/pages/main/main.tsx
@@ -3,16 +3,17 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "../../store";
 
+import { fetchCartApi } from "../../api/api";
 import { cartDataError, cartDataRequest, cartDataSuccess, clearProduct } from "../../store/actions/shop";
 import { ShopItem } from "../../components/shop-item/shop-item";
+import { ShowErrorOrLoading } from "../../components/show-error-or-loading/show-error-or-loading";
 
 import styles from "./main.module.css";
-import { fetchCartApi } from "../../api/api";
 
 export const Main = () => {
   const dispatch = useDispatch();
   const { authToken, authenticated, isUser } = useSelector((state) => state.auth);
-  const { products } = useSelector((state) => state.shop);
+  const { products, productsLoading, productsError } = useSelector((state) => state.shop);
 
   useEffect(() => {
     dispatch(clearProduct());
@@ -26,21 +27,23 @@ export const Main = () => {
 
   return (
     <main className={styles.main}>
-      <div className={styles.container}>
-        {products.length > 0 &&
-          products.map((product) => {
-            return (
-              <ShopItem
-                id={product.id}
-                img={product.picture}
-                name={product.name}
-                price={product.price}
-                inStock={product.inStock}
-                key={product.id}
-              />
-            );
-          })}
-      </div>
+      <ShowErrorOrLoading loading={productsLoading} error={productsError}>
+        <div className={styles.container}>
+          {products.length > 0 &&
+            products.map((product) => {
+              return (
+                <ShopItem
+                  id={product.id}
+                  img={product.picture}
+                  name={product.name}
+                  price={product.price}
+                  inStock={product.inStock}
+                  key={product.id}
+                />
+              );
+            })}
+        </div>
+      </ShowErrorOrLoading>
     </main>
   );
 };
