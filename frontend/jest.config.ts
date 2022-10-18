7c260a79c1ec5a5c6ab8af4e02ee7979f20db057
maslomeister <maslomeister@gmail.com>
2022-10-18 21:26:08 +0400

    adjusted jest config

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -35,20 +35,15 @@ export default {
   coverageProvider: "v8",
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  coverageReporters: ["text", "text-summary", "lcov", "clover"],
 
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 70,
-      functions: 70,
-      lines: 70,
-      statements: 70,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
 
