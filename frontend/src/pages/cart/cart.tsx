defd079491179ac58e2f089a7692221355f08de8
maslomeister <maslomeister@gmail.com>
2022-10-19 10:26:01 +0400

    feat: cart component tests

--- frontend/src/pages/cart/cart.tsx
+++ frontend/src/pages/cart/cart.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { useSelector } from "../../store";
+import { useAppSelector, useAppDispatch } from "../../store";
 
 import { clearCartApi } from "../../api/api";
 import { clearCart } from "../../store/actions/shop";
 import { Button } from "../../components/button/button";
 import { CartItem } from "../../components/cart-item/cart-item";
+import { ShowErrorOrLoading } from "../../components/show-error-or-loading/show-error-or-loading";
 
 import styles from "./cart.module.css";
 
@@ -15,9 +15,9 @@ interface IProps {
 }
 
 export const Cart: React.FC<IProps> = ({ showFooter }) => {
-  const dispatch = useDispatch();
-  const { authToken } = useSelector((state) => state.auth);
-  const { cart, cartLoading, cartTotal } = useSelector((state) => state.shop);
+  const dispatch = useAppDispatch();
+  const { authToken } = useAppSelector((state) => state.auth);
+  const { cart, cartLoading, cartError, cartTotal } = useAppSelector((state) => state.shop);
 
   const [clearCartState, setClearCartState] = useState({
     loading: false,
@@ -62,53 +62,51 @@ export const Cart: React.FC<IProps> = ({ showFooter }) => {
   return (
     <main className={styles.cart}>
       <h2>Корзина</h2>
-      {!cartLoading && (
-        <>
-          {cart.length === 0 ? (
-            <div className={styles.empty}>
-              <h2>Тут пока пусто</h2>
-            </div>
-          ) : (
-            <table className={styles["fl-table"]}>
-              <thead>
-                <tr>
-                  <th className={styles.id}>id</th>
-                  <th className={styles.name}>Имя</th>
-                  <th className={styles.price}>Цена</th>
-                  <th className={styles.amount}>Кол-во</th>
-                  <th className={styles.total}>Итог</th>
-                  <th className={styles.delete}></th>
-                </tr>
-              </thead>
-              <tbody>
-                {cart.map((item) => (
-                  <CartItem
-                    id={item.id}
-                    name={item.name}
-                    price={item.price}
-                    quantity={item.quantity}
-                    total={item.totalPrice}
-                    key={item.id}
-                  />
-                ))}
-              </tbody>
-            </table>
-          )}
-          {cart.length > 0 && (
-            <footer className={styles["controls-container"]}>
-              <div className={styles.controls}>
-                <Button loading={clearCartState.loading} error={clearCartState.error} onClick={onClearCartHandler}>
-                  Отчистить корзину
-                </Button>
-                <div className={styles.pay}>
-                  <p>Итог:{cartTotal}$</p>
-                  <Button inactive={true}>Оплатить</Button>
-                </div>
+      <ShowErrorOrLoading loading={cartLoading} error={cartError} dataTestId="cart-spinner">
+        {cart.length === 0 ? (
+          <div className={styles.empty}>
+            <h2>Тут пока пусто</h2>
+          </div>
+        ) : (
+          <table className={styles["fl-table"]}>
+            <thead>
+              <tr>
+                <th className={styles.id}>id</th>
+                <th className={styles.name}>Имя</th>
+                <th className={styles.price}>Цена</th>
+                <th className={styles.amount}>Кол-во</th>
+                <th className={styles.total}>Итог</th>
+                <th className={styles.delete}></th>
+              </tr>
+            </thead>
+            <tbody>
+              {cart.map((item) => (
+                <CartItem
+                  id={item.id}
+                  name={item.name}
+                  price={item.price}
+                  quantity={item.quantity}
+                  total={item.totalPrice}
+                  key={item.id}
+                />
+              ))}
+            </tbody>
+          </table>
+        )}
+        {cart.length > 0 && (
+          <footer className={styles["controls-container"]}>
+            <div className={styles.controls}>
+              <Button loading={clearCartState.loading} error={clearCartState.error} onClick={onClearCartHandler}>
+                Отчистить корзину
+              </Button>
+              <div className={styles.pay}>
+                <p>Итог:{cartTotal}$</p>
+                <Button inactive={true}>Оплатить</Button>
               </div>
-            </footer>
-          )}
-        </>
-      )}
+            </div>
+          </footer>
+        )}
+      </ShowErrorOrLoading>
     </main>
   );
 };
