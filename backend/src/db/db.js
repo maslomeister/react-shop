861e8cf6860d55afbe0b2edbccaa25e00b2a6d4d
maslomeister <maslomeister@gmail.com>
2022-10-21 14:22:24 +0400

    Updates

--- backend/src/db/db.js
+++
@@ -1,7 +0,0 @@
-const { JsonDB, Config } = require("node-json-db");
-
-const db = new JsonDB(new Config("database", true, false, "/"));
-
-module.exports = {
-  db,
-};
--- backend/src/routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-router.use("/products", require("./products"));
-router.use("/auth", require("./auth"));
-router.use("/cart", require("./cart"));
-
-module.exports = router;
